d325fdf5f84b438ea298f94bc196c9c3bca346ab
Gordon Campbell <gordy2007@hotmanil.com>
2025-10-07 20:25:18 +0100

    correction

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y distrobox  tailscale 
+dnf5 install -y distrobox  tailscale gnome-tweaks 
 dnf5 install -y --allowerasing \
     ffmpeg \
     ffmpegthumbnailer \
