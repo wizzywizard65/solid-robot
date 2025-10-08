4b3dab68a8c227b897044ae26d2c9ac8fa45701d
Gordon Campbell <gordy2007@hotmanil.com>
2025-10-08 11:46:28 +0100

    try again

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+dnf5 install  -y https://packages.microsoft.com/yumrepos/vscode
+dnf5 install  -y https://download.docker.com/linux/fedora/$releasever/$basearch/stable
+dnf5 install  -y https://download.docker.com/linux/fedora/$releasever/$basearch/test
+
 # this installs a package from fedora repos
-dnf5 install -y  install tmux code docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin distrobox gnome-tweaks
+dnf5 install -y  tmux code docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin distrobox gnome-tweaks
+
+
+
  
 # Use a COPR Example:
 #
@@ -22,6 +29,7 @@ dnf5 install -y  install tmux code docker-buildx-plugin docker-ce docker-ce-cli 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-systemctl enable docker.socket
+
+
 
 
