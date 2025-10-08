89a70a6f9d2de4c583272a24ad338d9b4f912b65
Gordon Campbell <gordy2007@hotmanil.com>
2025-10-08 11:40:26 +0100

    try again

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y  install tmux 
+dnf5 install -y  install tmux code docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin distrobox gnome-tweaks
+ 
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -21,5 +22,6 @@ dnf5 install -y  install tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable docker.socket
 
 
