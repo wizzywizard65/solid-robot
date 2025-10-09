7f772e47a0b39ced7e2ba48ceaf79a2c36017773
Gordon Campbell <gordy2007@hotmanil.com>
2025-10-09 21:10:59 +0100

    added tailscale

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 
 
 # this installs a package from fedora repos
-dnf5 install -y  tmux code gnome-extensions gnome-tweaks distrobox docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin flatpak-builder podman-compose podman-machine podman-tui podmansh fastfetch btop gnome-shell-extension-dash-to-dock virt-manager 
+dnf5 install -y  tmux code gnome-extensions gnome-tweaks distrobox docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin flatpak-builder podman-compose podman-machine podman-tui podmansh fastfetch btop gnome-shell-extension-dash-to-dock virt-manager tailscale 
 
 dnf5 remove -y firefox
 
@@ -36,6 +36,7 @@ dnf5 -y copr disable gmaglione/podman-bootc
 
 systemctl enable podman.socket
 systemctl enable docker.socket
+systemctl enable tailscaled
 
 
 dnf clean all
